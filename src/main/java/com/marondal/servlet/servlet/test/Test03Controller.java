f007047deed1700408e544c06fdba2f51b2d69dc
megaIt202 <megait202@gmailcom>
2024-01-29 18:26:42 +0900

    [김인규]  - Servlet 프로젝트 최초 파일 추가

---
+++ src/main/java/com/marondal/servlet/servlet/test/Test03Controller.java
@@ -0,0 +1,28 @@
+package com.marondal.servlet.servlet.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/test03")
+public class Test03Controller extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><head><title>[단독]고양이가 야옹해</title></head><body>");
+		out.println("<h1>[단독] 고양이가 야옹해</h1>");
+		out.println("<div>기사입력 시간 : 2024/01/25 16:09:42</div>");
+		out.println("<hr><div>끝</div></body></html>");
+	}
+
+}
